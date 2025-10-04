a94b661f99b7c8b5a58e652261a2e3d2f233852f
Hrvoje <hrvoje.zeman1@gmail.com>
2025-10-04 21:34:03 +0200

    Hashed file list

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import \
+	hashlib
 import os
 import sys
 
@@ -8,10 +10,22 @@ class File:
 	def __init__(self, FilePath):
 		self.FilePath = FilePath
 		self.FileID = os.popen(fr"fsutil file queryfileid 'C:\Users\User\Desktop\Copier\test' ").read()
+		self.FileHash = self.get_file_hash()
 
 	def get_file_ID(self, FilePath):
 		return os.popen(fr"fsutil file queryfileid {FilePath}").read().split(' ')[-1].replace('\n', '')
 
+	def get_file_hash(self):
+		hash_md5 = hashlib.md5()
+		with open(self.FilePath, "rb") as f:
+			for chunk in iter(lambda: f.read(4096), b""):
+				hash_md5.update(chunk)
+		return hash_md5.hexdigest()
 
+
+OldHashedList = []
+NewHashedList = []
 while True:
-	if 
+	if OldHashedList == []: # Populate the list for the first time
+		for file in os.listdir(r"C:\Users\User\Desktop\Copier\test"):
+			OldHashedList.append(File.get_file_hash())
